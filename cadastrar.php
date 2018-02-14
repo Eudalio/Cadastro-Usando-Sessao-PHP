abd71e9291a62c06e4fa8a276514169884a998c5
eudalio <tonnydallios@gmail.com>
2018-02-13 21:54:49 -0300

    Formulário da página cadastrar pronto

--- cadastrar.php
+++ cadastrar.php
@@ -1,6 +1,8 @@
-
+<?php 
+	require_once('estados.php');
+?>
 <!doctype html>
-<html lang="en">
+<html lang="pt-BR">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -36,7 +38,48 @@
     <main role="main" class="container">
 
       <div class="starter-template">
-        
+	  		<!-- TODO ACTION -->
+				<form action="" method="post" style="text-align: left;" >
+					<div class="form-group">
+						<label for="nome">nome</label>
+						<input type="text" class="form-control" name="nome" placeholder="Nome da pessoa" id="nome" value="">
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-4">
+							<label for="idade">Idade</label>
+							<input type="number" class="form-control" name="idade" placeholder="Idade da pessoa" id="idade" value="">
+						</div>
+						<div class="form-group col-md-8">
+							<label for="telefone">Telefone</label>
+							<input type="text" class="form-control" name="telefone" placeholder="Telefone da pessoa" id="telefone" value="">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="endereco">Endereco</label>
+						<input type="text" class="form-control" name="endereco" placeholder="Endereco da pessoa" id="endereco" value="">
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-8">
+							<label for="cidade">Cidade</label>
+							<input type="text" class="form-control" name="cidade" placeholder="cidade da pessoa" id="cidade" value="">
+						</div>
+						<div class="form-group col-md-4">
+							<label for="estado">Estado</label>
+							<select name="estado" id="estado" class="form-control">
+<?php 
+								foreach($estados as $i => $uf){
+									echo "<option value='$i'>" . $uf . "</option>";
+								}							
+							
+?>
+								</select>
+						</div>
+					</div>
+					<br>
+					<br>
+					<button type="submit" class="btn btn-primary">Salvar</button>
+					<button type="reset" class="btn btn-warning">Limpar</button>
+				</form>
       </div>
 
     </main><!-- /.container -->
---
+++ estados.php
@@ -0,0 +1,3 @@
+<?php 
+    $estados = ['SP', 'RJ', 'SC', 'MT', 'BA', 'SE', 'PE', 'CE', 'AL', 'MA', 'RS', 'AM', 'GO', 'AC', 'ES', 'RO'];
+?>
